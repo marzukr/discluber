039aea6e821b3d9f84ff23c8849cc89dc5792f33
Marzuk Rashid <marzukr@gmail.com>
2017-10-13 00:09:53 -0500

    regex twitter handle validation

--- app/views.py
+++ app/views.py
@@ -4,9 +4,53 @@ from app import app
 from ClubEngine import clubsPython3
 import tweepy
 
-@app.route('/')
+import re
+
+"""
+HTTP ERROR HANDLER
+"""
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
+
+@app.route('/api/recommend', methods=['GET'])
+def clubReccomendation():
+    if re.fullmatch("^@?[a-zA-Z_0-9]{1,15}", request.args["twitterUsername"]):
+        # try:
+        #     username = request.form['username']
+        #     clubs = clubsPython3.returnResults(username)
+        #     return jsonify({'clubs' : clubs})
+        # except tweepy.TweepError as e:
+        #     message = str(e)
+        #     code = ""
+        #     for letter in message:
+        #         if letter.isdigit():
+        #             code = code + letter
+        #     return jsonify({'error' : message, 'code' : code})
+        return request.args["twitterUsername"] + " is a valid twitter handle"
+    else:
+        raise InvalidUsage("Invalid twitter username", 400)
 
 # @app.route('/process', methods=['POST'])
 # def process():
