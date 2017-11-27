c4fbffe6d0e63a772d0c8589bc61b2a9c6d72b27
Marzuk Rashid <marzukr@gmail.com>
2017-11-27 00:17:55 -0600

    tfidf database update function complete

--- ClubEngine/clubsPython3.py
+++ ClubEngine/clubsPython3.py
@@ -33,7 +33,7 @@ auth.set_access_token(access_key, access_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-dbFunctions.runFunction(tweetsCollection)
+dbFunctions.runFunction(db.testFreqs, tweetsCollection)
 
 def search(uri, term):
     #Take the term and run it through elasticsearch
--- ClubEngine/dbFunctions.py
+++ ClubEngine/dbFunctions.py
@@ -1,9 +1,9 @@
 import tfidfEngine
 
-from timeit import default_timer as timer
-
-def clubTweetTokens(mongoCollection):
-    for clubItem in mongoCollection.find({}):
+def clubTweetTokens(tweetCollection):
+    count = 0
+    total = tweetCollection.count()
+    for clubItem in tweetCollection.find({}):
         tokens = []
         for tweetAgg in clubItem["TweetsString"]:
             tokens += tfidfEngine.preprocess(tweetAgg, True)
@@ -11,14 +11,21 @@ def clubTweetTokens(mongoCollection):
         for uToken in uTokens:
             yield uToken
 
-def runFunction(mongoCollection):
-    start = timer()
-    generator = clubTweetTokens(mongoCollection)
-    # print(next(generator))
-    count = 0
-    for i in generator:
-        # print(next(generator))
         count += 1
-    print(count)
-    end = timer() 
-    print("Time taken:", end - start)
+        progbar(count, total, 40, "Processing: " + clubItem["Club Name"])
+
+def storeDocumentFreq(freqCollection, tweetCollection):
+    for token in clubTweetTokens(tweetCollection):
+        freqCollection.update_one({"Term": token}, {"$inc": {"df": 1}}, upsert=True)
+
+def runFunction(freqCollection, tweetCollection):
+    storeDocumentFreq(freqCollection, tweetCollection)
+    # for i in clubTweetTokens(tweetCollection):
+    #     nothing = None
+
+def progbar(curr, total, full_progbar, status=""):
+    frac = curr/total
+    filled_progbar = round(frac*full_progbar)
+    print('\033[K' + "\t" + '#'*filled_progbar + '-'*(full_progbar-filled_progbar) + "[{0:.2f}%]".format(frac*100) + " " + status, end='\r')
+    if curr == total:
+        print('\033[K')
