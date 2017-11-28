1361f7d8ca391f79f52de873499cbc5c0711f103
Marzuk Rashid <marzukr@gmail.com>
2017-11-27 20:43:35 -0600

    optimized document freq calculation

--- ClubEngine/dbFunctions.py
+++ ClubEngine/dbFunctions.py
@@ -1,31 +1,41 @@
 import tfidfEngine
+from tqdm import tqdm
+from collections import Counter
 
-def clubTweetTokens(tweetCollection):
-    count = 0
-    total = tweetCollection.count()
-    for clubItem in tweetCollection.find({}):
+def clubTweetTokens(tweetCollection, returnLength=False):
+    length = 0
+    clubItemIDS = [clubItem["_id"] for clubItem in tweetCollection.find({})]
+    for clubItemID in clubItemIDS:
+        clubItem = tweetCollection.find_one({"_id": clubItemID})
         tokens = []
         for tweetAgg in clubItem["TweetsString"]:
             tokens += tfidfEngine.preprocess(tweetAgg, True)
         uTokens = list(set(tokens))
-        for uToken in uTokens:
-            yield uToken
-
-        count += 1
-        progbar(count, total, 40, "Processing: " + clubItem["Club Name"])
+        if returnLength:
+            length += len(uTokens)
+        else:
+            for uToken in uTokens:
+                yield uToken
+    if returnLength:
+        yield length
 
 def storeDocumentFreq(freqCollection, tweetCollection):
+    # totalTokens = next(clubTweetTokens(tweetCollection, returnLength=True))
+    totalTokens = 2500000
+    pbar = tqdm(total=totalTokens)
+    tokenCounter = Counter()
     for token in clubTweetTokens(tweetCollection):
-        freqCollection.update_one({"Term": token}, {"$inc": {"df": 1}}, upsert=True)
+        # freqCollection.update_one({"Term": token}, {"$inc": {"df": 1}}, upsert=True)
+        # freqCollection.insert_one({"Term": token})
+        tokenCounter.update([token])
+        pbar.update(1)
+    pbar.close()
+
+    pbar = tqdm(total=len(tokenCounter))
+    for token, freq in tokenCounter.items():
+        freqCollection.insert_one({"Term": token, "df": freq})
+        pbar.update(1)
+    pbar.close()
 
 def runFunction(freqCollection, tweetCollection):
     storeDocumentFreq(freqCollection, tweetCollection)
-    # for i in clubTweetTokens(tweetCollection):
-    #     nothing = None
-
-def progbar(curr, total, full_progbar, status=""):
-    frac = curr/total
-    filled_progbar = round(frac*full_progbar)
-    print('\033[K' + "\t" + '#'*filled_progbar + '-'*(full_progbar-filled_progbar) + "[{0:.2f}%]".format(frac*100) + " " + status, end='\r')
-    if curr == total:
-        print('\033[K')
